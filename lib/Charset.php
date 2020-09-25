ad0a8ae27ab319488cb7f0238b43d2b6a115ffd2
J. King <jking@jkingweb.ca>
2020-09-25 12:34:53 -0400

    Replace Content-Type parser with proper version

--- lib/Charset.php
+++ lib/Charset.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 namespace dW\HTML5;
 
 use MensBeam\Intl\Encoding;
+use MensBeam\Mime\MimeType;
 
 abstract class Charset {
     /** Finds a Unicode byte order mark by a byte stream 
@@ -37,58 +38,11 @@ abstract class Charset {
      * @param string $contentType The value of a Content-Type header-field
      */
     public static function fromTransport(string $contentType): ?string {
-        // Try to sniff out a charset from a Content-Type header-field.
-        // This does cut some corners, but should be sufficient for practical use
-        $s = preg_replace("/\s+/", " ", strtolower($contentType));
-        $pos = 0;
-        $end = strlen($s);
-        // skip the type
-        while ($pos < $end && @$s[$pos++] !== "/");
-        // skip the subtype
-        while ($pos < $end && @$s[$pos++] !== ";");
-        // check parameters in sequence
-        while ($pos < $end) {
-            // skip any leading whitespace
-            if (@$s[$pos] === " ") {
-                $pos++;
-            }
-            // collect characters for the parameter name
-            $param = "";
-            while ($pos < $end && @$s[$pos] !== "=") {
-                $param .= @$s[$pos++];
-            }
-            // skip the equals sign
-            $pos++;
-            if ($s[$pos] === '"') {
-                // Value is a quoted-string
-                $pos++;
-                $value = "";
-                while (!in_array($c = @$s[$pos++], ['"', ""])) {
-                    if ($c === "\\") {
-                        $value .= @$s[$pos++];
-                    } else {
-                        $value .= $c;
-                    }
-                }
-                // only interpret the value if a closing quotation mark was seen
-                if ($c !== '"') {
-                    $value = "";
-                }
-            } else {
-                // Value is a bare token
-                $value = "";
-                while (!in_array($c = @$s[$pos++], [';', " ", ""])) {
-                    $value .= $c;
-                }
-            }
-            // if the parameter was the character set, interpret its value and return
-            if ($param === "charset") {
-                $encoding = Encoding::matchLabel($value);
-                if ($encoding) {
-                    return $encoding['name'];
-                } else {
-                    return null;
-                }
+        $type = MimeType::parseBytes($contentType);
+        if ($type && isset($type->params['charset'])) {
+            $encoding = Encoding::matchLabel($type->params['charset']);
+            if ($encoding) {
+                return $encoding['name'];
             }
         }
         return null;
