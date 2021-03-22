28525a2c9fe21ed567b62e8d60fe2e28119caee9
J. King <jking@jkingweb.ca>
2021-03-22 10:26:28 -0400

    Don't claim copyright on machine-generated data
    Note that the pattern has changed because sorting  differs in PHP 8, not
    because of a specification change

--- RoboFile.php
+++ RoboFile.php
@@ -162,10 +162,6 @@ class RoboFile extends \Robo\Tasks {
     public function charref() {
         $template = <<<'FILE'
 <?php
-/** @license MIT
- * Copyright 2017 , Dustin Wilson, J. King et al.
- * See LICENSE and AUTHORS files for details */
-
 declare(strict_types=1);
 namespace MensBeam\HTML;
 
